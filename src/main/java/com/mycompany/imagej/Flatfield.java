d964e3865501d3906dc9e6538a881c4eca6bb14a
Lars Berntzon <lars.berntzon@cecilia-data.se>
2020-02-20 00:23:04 +0100

    First attempt

--- src/main/java/com/mycompany/imagej/Flatfield.java
+++ src/main/java/com/mycompany/imagej/Flatfield.java
@@ -21,7 +21,7 @@ import ij.process.ImageProcessor;
  *
  * @author Johannes Schindelin
  */
-public class Process_Pixels implements PlugInFilter {
+public class Flatfield implements PlugInFilter {
 	protected ImagePlus image;
 
 	// image property members
@@ -29,8 +29,9 @@ public class Process_Pixels implements PlugInFilter {
 	private int height;
 
 	// plugin parameters
-	public double value;
-	public String name;
+	public double z0;
+	public double z1;
+	public double z2;
 
 	@Override
 	public int setup(String arg, ImagePlus imp) {
@@ -56,19 +57,21 @@ public class Process_Pixels implements PlugInFilter {
 	}
 
 	private boolean showDialog() {
-		GenericDialog gd = new GenericDialog("Process pixels");
+		GenericDialog gd = new GenericDialog("Generate flatfield");
 
 		// default value is 0.00, 2 digits right of the decimal point
-		gd.addNumericField("value", 0.00, 2);
-		gd.addStringField("name", "John");
+		gd.addNumericField("z⁰ factor:", 0.00, 2);
+		gd.addNumericField("z¹ factor:", 0.00, 2);
+		gd.addNumericField("z² factor:", 0.00, 2);
 
 		gd.showDialog();
 		if (gd.wasCanceled())
 			return false;
 
 		// get entered values
-		value = gd.getNextNumber();
-		name = gd.getNextString();
+		z0 = gd.getNextNumber();
+		z1 = gd.getNextNumber();
+		z2 = gd.getNextNumber();
 
 		return true;
 	}
@@ -97,67 +100,27 @@ public class Process_Pixels implements PlugInFilter {
 	// Select processing method depending on image type
 	public void process(ImageProcessor ip) {
 		int type = image.getType();
-		if (type == ImagePlus.GRAY8)
-			process( (byte[]) ip.getPixels() );
-		else if (type == ImagePlus.GRAY16)
-			process( (short[]) ip.getPixels() );
-		else if (type == ImagePlus.GRAY32)
+		if (type == ImagePlus.GRAY32)
 			process( (float[]) ip.getPixels() );
-		else if (type == ImagePlus.COLOR_RGB)
-			process( (int[]) ip.getPixels() );
 		else {
-			throw new RuntimeException("not supported");
+			throw new RuntimeException("not supported: " + Integer.toString(type));
 		}
 	}
 
-	// processing of GRAY8 images
-	public void process(byte[] pixels) {
-		for (int y=0; y < height; y++) {
-			for (int x=0; x < width; x++) {
-				// process each pixel of the line
-				// example: add 'number' to each pixel
-				pixels[x + y * width] += (byte)value;
-			}
-		}
-	}
-
-	// processing of GRAY16 images
-	public void process(short[] pixels) {
-		for (int y=0; y < height; y++) {
-			for (int x=0; x < width; x++) {
-				// process each pixel of the line
-				// example: add 'number' to each pixel
-				pixels[x + y * width] += (short)value;
-			}
-		}
-	}
-
-	// processing of GRAY32 images
+	// processing of COLOR_RGB images
 	public void process(float[] pixels) {
 		for (int y=0; y < height; y++) {
 			for (int x=0; x < width; x++) {
 				// process each pixel of the line
 				// example: add 'number' to each pixel
-				pixels[x + y * width] += (float)value;
-			}
-		}
-	}
-
-	// processing of COLOR_RGB images
-	public void process(int[] pixels) {
-		for (int y=0; y < height; y++) {
-			for (int x=0; x < width; x++) {
-				// process each pixel of the line
-				// example: add 'number' to each pixel
-				pixels[x + y * width] += (int)value;
+				double z = Math.sqrt(Math.pow(x - width/2, 2) + Math.pow(y - height/2, 2));
+				pixels[x + y * width] += z0 + z1*z + z2*z*z;
 			}
 		}
 	}
 
 	public void showAbout() {
-		IJ.showMessage("ProcessPixels",
-			"a template for processing each pixel of an image"
-		);
+		IJ.showMessage("Flatfield", "a flatfield simulator");
 	}
 
 	/**
@@ -171,7 +134,7 @@ public class Process_Pixels implements PlugInFilter {
 	public static void main(String[] args) throws Exception {
 		// set the plugins.dir property to make the plugin appear in the Plugins menu
 		// see: https://stackoverflow.com/a/7060464/1207769
-		Class<?> clazz = Process_Pixels.class;
+		Class<?> clazz = Flatfield.class;
 		java.net.URL url = clazz.getProtectionDomain().getCodeSource().getLocation();
 		java.io.File file = new java.io.File(url.toURI());
 		System.setProperty("plugins.dir", file.getAbsolutePath());
@@ -180,7 +143,8 @@ public class Process_Pixels implements PlugInFilter {
 		new ImageJ();
 
 		// open the Clown sample
-		ImagePlus image = IJ.openImage("http://imagej.net/images/clown.jpg");
+		ImagePlus image = IJ.createImage("Flatfield", 300, 300, 1, 32);
+
 		image.show();
 
 		// run the plugin
