a66b0726d518c09ced1cff6c111a9867a6cc5b8e
Lars <Lars@Lars-work-pc>
2020-03-04 12:35:15 +0100

    Added a 6th degree factor

--- src/main/java/Flatfield_.java
+++ src/main/java/Flatfield_.java
@@ -25,12 +25,14 @@ public class Flatfield_ implements PlugIn {
 	private int opt_cent_x; // In pixels
 	private int opt_cent_y; // In pixels
 	
-	// plugin parameters
-	private double a; // polynomial, constants
-	private double b;
-	private double c;
-	private double d;
+	// plugin parameters polynomial
+	private double a; // constant
+	private double b; // not used yet, rotation symmetric
+	private double c; // 2th degree
+	private double d; // not used yet, rotation symmetric
 	private double e; // 4th degree
+	private double f; // not used yet, rotation symmetric
+	private double g; // 6th degree
 
 	@Override
 	public void run(String value) {
@@ -51,33 +53,30 @@ public class Flatfield_ implements PlugIn {
 	private boolean showDialog() {
 		GenericDialog gd = new GenericDialog("Flatfield");
 		
-		String[] exponentDef = {"-16","-15","-14","-13","-12","-11","-10","-09","-08","-07","-06","-05","-04","-03","-02","-01","0","+01","+02"};
-		// position                0     1     2     3     4     5     6     7     8     9    10    11    12    13    14    15  16    17    18
+		String[] exponentDef = {"-21","-20","-19","-18","-17","-16","-15","-14","-13","-12","-11","-10","-09","-08","-07","-06","-05","-04","-03","-02","-01","0","+01","+02"};
+		// position                0     1     2     3     4     5     6     7     8     9    10    11    12    13    14    15    16    17    18    19    20  21    22    23
 		
 		// default value is 0.00, 2 digits right of the decimal point
 		
 		gd.addMessage("Generate flat image from polynomial");
 		gd.addMessage("Flat image dimensions");
-		gd.addNumericField("Width:", 4500, 0, 6, "pixel");
-		gd.addNumericField("Height:", 3000, 0, 6, "pixel");
-		gd.addNumericField("Optical center X:", 2250, 0, 6, "pixel");
-		gd.addNumericField("Optical center Y:", 1500, 0, 6, "pixel");
+		gd.addNumericField("Width:", 2238, 0, 6, "pixel");
+		gd.addNumericField("Height:", 1477, 0, 6, "pixel");
+		gd.addNumericField("Optical center X:", 1100, 0, 6, "pixel");
+		gd.addNumericField("Optical center Y:", 730, 0, 6, "pixel");
 		
 		gd.addMessage("Polynomial constants");
-		gd.addNumericField("a bas: ", +1.0, 5, 7, "* 10^exp * r^0"); // should be normalized to 1 in center !gd.addToSameRow();
-		gd.addChoice("a exp: ", exponentDef, exponentDef[16]);
-				
-		gd.addNumericField("b bas: ", 0.0, 5, 7, "* 10^exp * r^1, normally not used");
-		gd.addChoice("b exp: ", exponentDef, exponentDef[16]);
-				
-		gd.addNumericField("c bas:", -1.4, 5, 7, "* 10^exp * r^2");
-		gd.addChoice("c exp: ", exponentDef, exponentDef[9]);
-				
-		gd.addNumericField("d bas: ", +0.0, 5, 7, "* 10^exp * r^3,  normally not used");
-		gd.addChoice("d exp: ", exponentDef, exponentDef[16]);
-				
-		gd.addNumericField("e bas: ", -1.3, 5, 7, "* 10^exp * r^4");
-		gd.addChoice("e exp: ", exponentDef, exponentDef[2]);
+		gd.addNumericField("a bas: ", +9.65, 5, 7, "* 10^exp * r^0"); // should be normalized to 1 in center !gd.addToSameRow();
+		gd.addChoice("a exp: ", exponentDef, exponentDef[20]);
+						
+		gd.addNumericField("c bas:", -9.4, 5, 7, "* 10^exp * r^2");
+		gd.addChoice("c exp: ", exponentDef, exponentDef[14]);
+						
+		gd.addNumericField("e bas: ", +1.3, 5, 7, "* 10^exp * r^4");
+		gd.addChoice("e exp: ", exponentDef, exponentDef[9]);
+		
+		gd.addNumericField("d bas: ", -6.7, 5, 7, "* 10^exp * r^6, only in difficult case");
+		gd.addChoice("d exp: ", exponentDef, exponentDef[2]);
 				
 		gd.addMessage("Choose parameters to get it normalized to =1 in center");
 		gd.addMessage("Values lower than 0.3, i.e. vignetting of 70% will be cut");
@@ -100,25 +99,27 @@ public class Flatfield_ implements PlugIn {
 		double a_bas = gd.getNextNumber();
 		String a_exp = gd.getNextChoice();
 		a = a_bas * Math.pow(10, Double.parseDouble(a_exp));
-		
-		double b_bas = gd.getNextNumber();
-		String b_exp = gd.getNextChoice();
-		b = b_bas * Math.pow(10, Double.parseDouble(b_exp));
+			
+		b = 0; // spare part for future maybe
 				
 		double c_bas = gd.getNextNumber();
 		String c_exp = gd.getNextChoice();
 		c = c_bas * Math.pow(10, Double.parseDouble(c_exp));
 		
-		double d_bas = gd.getNextNumber();
-		String d_exp = gd.getNextChoice();
-		d = d_bas * Math.pow(10, Double.parseDouble(d_exp));
+		d = 0; // spare part for future maybe
 				
 		double e_bas = gd.getNextNumber();
 		String e_exp = gd.getNextChoice();
 		e = e_bas * Math.pow(10, Double.parseDouble(e_exp));
 		
+		d = 0; // spare part for future maybe
+		
+		double g_bas = gd.getNextNumber();
+		String g_exp = gd.getNextChoice();
+		g = g_bas * Math.pow(10, Double.parseDouble(g_exp));
+		
 		// Check calculations:
-		System.out.println("A constant = " + a + ", B constant = " + b + ", C constant = " + c + ", D constant = " + d + ", E constant = " + e);
+		System.out.println("A constant = " + a + ", B constant = " + b + ", C constant = " + c + ", D constant = " + d + ", E constant = " + e + ", F constant = " + f + ", G constant = " + g);
 		
 		return true;
 	}
@@ -160,13 +161,17 @@ public class Flatfield_ implements PlugIn {
 		for (int y=0; y < height; y++) {
 			for (int x=0; x < width; x++) {
 				double radie = Math.sqrt(Math.pow(x - opt_cent_x, 2) + Math.pow(y - opt_cent_y, 2)); // only integers
-				float level = (float)(a + b*radie + c*Math.pow(radie, 2) + d*Math.pow(radie, 3) + e*Math.pow(radie, 4));
+				float level = (float)(a + c*Math.pow(radie, 2) + e*Math.pow(radie, 4) + g*Math.pow(radie, 6));
 				if (level < 0.3f) { // no correction of abnorm vignetting
 					level = 0.3f;
 				}
-				pixels[x + y * width] = level; // built flat image with 32 float
+				pixels[x + y * width] = level; // built flat image with 32 bit float
 			}
 		}
+		
+		// Check calculations radius:
+		double radius = Math.sqrt(Math.pow(0 - opt_cent_x, 2) + Math.pow(730 - opt_cent_y, 2));
+		System.out.println("Radiou at x=0 = " + radius);
 	}
 
 	public void showAbout() {
